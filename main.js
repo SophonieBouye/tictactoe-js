3d9c2f1027453cf348259ed090f9a55fdfbb94c6
Samuel Bouye <bouyesamuel@gmail.com>
2022-05-30 17:14:01 +0000

    get id of cell

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+let cells = document.querySelectorAll("td")
+
+function getCellId(e) {
+    let id = e.target.attributes['id'].value;
+    console.log(id)
+}
+
+for(let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', getCellId, false)
+}
