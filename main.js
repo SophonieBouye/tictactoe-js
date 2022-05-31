501db530c07a7a1f068af996ccd7c260c755ea8b
Samuel Bouye <bouyesamuel@gmail.com>
2022-05-31 14:42:49 +0000

    a player cannot reuser a square who are already taken

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 let cells = document.querySelectorAll("td")
 
+const PLAYER_X = 'X'
+const PLAYER_O = 'O'
+let current = false
+
 function getCellId(e) {
     let id = e.target.attributes['id'].value;
-    console.log(id)
+
+    mark(id)
 }
 
 for(let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', getCellId, false)
+}
+
+function mark(id) {
+    
+    let cell = document.getElementById(id)
+    
+    if(cell.innerText == "") {
+        cell.innerText = current ? PLAYER_X : PLAYER_O
+        switchPlayer();
+    } else {
+        console.log("already taken")
+    }
+}
+
+
+function switchPlayer(){
+    return current = current ? false : !current
 }
