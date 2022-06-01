9a74e4dbe8583039c244c302ab40e765e2807c19
Samuel Bouye <bouyesamuel@gmail.com>
2022-06-01 12:19:17 +0000

    check what square are selected and get id

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ let current = false
 
 function getCellId(e) {
     let id = e.target.attributes['id'].value;
-
     mark(id)
 }
 
@@ -15,18 +14,39 @@ for(let i = 0; i < cells.length; i++) {
 }
 
 function mark(id) {
-    
     let cell = document.getElementById(id)
     
     if(cell.innerText == "") {
         cell.innerText = current ? PLAYER_X : PLAYER_O
         switchPlayer();
-    } else {
-        console.log("already taken")
     }
+    let x_result = Array.from(cells).filter(x => x.innerText == 'X');
+    let o_result = Array.from(cells).filter(o => o.innerText == 'O');
+    console.log('x', x_result);
+    console.log('o', o_result);
+    
 }
-
 
 function switchPlayer(){
     return current = current ? false : !current
 }
+
+const winCombinaison = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function winner() {
+    
+    // winCombinaison.forEach(comb => {
+    //     console.log(comb);
+    // });
+}
+
+winner()
