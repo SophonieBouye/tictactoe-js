136353213fa0a465e5be5d3e7c5768ed9c9955bb
Samuel Bouye <bouyesamuel@gmail.com>
2022-06-07 11:39:18 +0000

    tic tac toe work fine

--- main.js
+++ main.js
@@ -1,54 +1,10 @@
-let cells = document.querySelectorAll("td")
-
+let cells = Array.from(document.querySelectorAll(".cell"))
+let board = document.querySelector(".board")
 const PLAYER_X = 'X'
 const PLAYER_O = 'O'
-let current = false
-let cellX = []
-let cellO = []
-
-// Get Id of cell 
-function getCellId(e) {
-    let id = e.target.attributes['id'].value;
-    mark(id)
-}
-
-for(let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', getCellId, false)
-}
-
-
-//Marks function
-function mark(id) {
-    let cell = document.getElementById(id)
-    
-    if(cell.innerText == "") {
-        cell.innerText = current ? PLAYER_X : PLAYER_O
-        switchPlayer();
-    }
-
-    cells.forEach(cell => {
-        if(cell.innerText == "O"){
-            if(!cellO.find(el => el == cell.attributes['id'].value)){
-                cellO.push(cell.attributes['id'].value)
-            }
-        }else {
-            if(!cellX.find(el => el == cell.attributes['id'].value)){
-                cellX.push(cell.attributes['id'].value)
-            }
-        }
-
-    });
-    winner(cellO)
-    // let x_result = Array.from(cells).filter(x => x.innerText == 'X' ? x.attributes['id'] : '');
-    // let o_result = Array.from(cells).filter(o => o.innerText == 'O');
-    // console.log('x', x_result);
-    // console.log('o', o_result);
-}
-
-function switchPlayer(){
-    return current = current ? false : !current
-}
-
+let winner = ""
+let winnerMessage = ""
+let currentPlayer = PLAYER_X
 const winCombinaison = [
     [0, 1, 2],
     [3, 4, 5],
@@ -60,23 +16,31 @@ const winCombinaison = [
     [2, 4, 6]
 ]
 
-function winner(result) {
-
-    result = result.map(x => Number(x))
-    if(result.length > 2) {
-        for(let i = 0; i < winCombinaison.length; i++) {
-              for (let y = 0; y < result.length; y++) {
-                  console.log(winCombinaison.filter(w => {
-                      result.includes(w)
-                  }))
-              }
-        }
-
-    }
+function checkWinner(winnerCombinaison, currentPlayer) {
+    winnerCombinaison.forEach(combinaison => {
+        let counter = 0
+        combinaison.forEach(element => {
+            if(cells[element].innerHTML == currentPlayer) {
+                counter++;
+                if(counter == 3 ) {
+                    winner = currentPlayer
+                    winnerMessage = `${winner} is the Winner`
+                    document.querySelector('.message').innerHTML = winnerMessage
+                }
+            }
+        })
+    })
 }
 
 
-// if(result.includes(winCombinaison[i][y])){
-//     console.log(winCombinaison[i], result);
-//     console.log('match find..', winCombinaison[i], result);
-// }
+//Move function
+function move(e) {
+    if(e.target.className == "cell" && e.target.innerHTML == "") {
+        e.target.innerHTML = currentPlayer;
+        checkWinner(winCombinaison, currentPlayer)
+        currentPlayer = (currentPlayer == PLAYER_X) ? PLAYER_O : PLAYER_X
+    }
+
+}
+
+board.addEventListener("click", move)
